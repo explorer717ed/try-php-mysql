2a2cd922c3a10bb4552738d5ddfcef5958ec1bc8
717 <explorer717ed@gmail.com>
2024-09-21 22:06:48 +1200

    ref: rm unused files

--- src/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-  <nav>
-    <ul>
-      <li class="inline"><a href="/index.php" class="text-green-700 underline">HOME</a></li>
-      <li class="inline"><a href="/formAdd.php" class="text-green-700 underline">ADD</a></li>
-    </ul>
-  </nav>
-</header>
--- src/index.php
+++ src/index.php
@@ -1,43 +1,10 @@
-<?php
-require 'connectDB.php';
-
-
-
-// if($result = $mysql->query($sql)){
-//   while($data = $result->fetch_object()){
-//     $works[] = $data;
-//   }
-// }
-
-// foreach ($works as $work) {
-//   echo "<br>";
-//   echo $work->title . " " . $work->projectType;
-//   echo "<br>";
-// }
-
-
-#### STRING ####
-$title = "Frontal Lobe";
-
-$slogan = "hello from \"dockereerer phppp myyyysql\""; //double quote and escape
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php echo $title ?></title>
-  <script src="https://cdn.tailwindcss.com"></script>
+  <title>PHP</title>
 </head>
 <body>
-  <?php require('header.php') ?>
-  <?php echo "<h1> $slogan </h1>"; ?>
-  <ul>
-    <li><a href="/review.php" class="text-blue-700 underline">REVIEW</a></li>
-    <li><a href="/pdo.php" class="text-blue-700 underline">PDO</a></li>
-  </ul>
-
 </body>
 </html>
